6daa9b479279c5ca50a260c0c5021d6575d581b5
james riady <jamesriady1998@gmail.com>
2020-05-14 17:26:04 +0700

    init

---
+++ precache-manifest.b77f72a365705e333003770523846af0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17366af53da185aad29391980de9c7a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c283626b50b8beb03c99",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a1a667c769bb9179692d",
+    "url": "/static/js/2.e1a0b1cd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e1a0b1cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c283626b50b8beb03c99",
+    "url": "/static/js/main.c0170f0c.chunk.js"
+  },
+  {
+    "revision": "30a1933ba384d34441a9",
+    "url": "/static/js/runtime-main.1557b767.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
